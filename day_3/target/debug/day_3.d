40407014baefb094f6a142ad9d2194dbe1d7f681
Nick Johnson <164326026+Phirks@users.noreply.github.com>
2024-10-21 19:22:54 -0400

    Delete day_3 directory

--- day_3/target/debug/day_3.d
+++
@@ -1,1 +0,0 @@
-/home/phirks/Documents/rust/advent_of_code/day_3/target/debug/day_3: /home/phirks/Documents/rust/advent_of_code/day_3/src/main.rs
--- day_3/target/debug/deps/day_3-1b6cd938f0f5d653.d
+++
@@ -1,5 +0,0 @@
-/home/phirks/Documents/rust/advent_of_code/day_3/target/debug/deps/day_3-1b6cd938f0f5d653: src/main.rs
-
-/home/phirks/Documents/rust/advent_of_code/day_3/target/debug/deps/day_3-1b6cd938f0f5d653.d: src/main.rs
-
-src/main.rs:
