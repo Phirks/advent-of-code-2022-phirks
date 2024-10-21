efe2022cc1b3c2804cf76cb97637a3654d40967c
Phirks <Phirks@outlook.com>
2024-10-21 19:37:22 -0400

    reseting rather than reverting

--- day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-C:\Rust\advent_2022\day_1\target\debug\day_1.exe: C:\Rust\advent_2022\day_1\src\main.rs
--- day_1/target/debug/deps/day_1-d901feb9c0881b47.d
+++ day_1/target/debug/deps/day_1-d901feb9c0881b47.d
@@ -1,5 +1,5 @@
-C:\Rust\advent_2022\day_1\target\debug\deps\libday_1-d901feb9c0881b47.rmeta: src/main.rs
+C:\Rust\advent-of-code-2022-phirks\day_1\target\debug\deps\libday_1-d901feb9c0881b47.rmeta: src/main.rs
 
-C:\Rust\advent_2022\day_1\target\debug\deps\day_1-d901feb9c0881b47.d: src/main.rs
+C:\Rust\advent-of-code-2022-phirks\day_1\target\debug\deps\day_1-d901feb9c0881b47.d: src/main.rs
 
 src/main.rs:
