735abe03000b3cc6854e8a4784684d35fb6f8b2d
Phirks <Phirks@outlook.com>
2024-10-21 19:33:58 -0400

    Revert "Delete day_2/target directory"
    This reverts commit 1bb0b368bd761a8a1de5ce7580db990b6c5c236a.

---
+++ day_2/target/debug/deps/day_2-933a1194455727ce.d
@@ -0,0 +1,5 @@
+/home/phirks/Documents/rust/advent_of_code/day_2/target/debug/deps/day_2-933a1194455727ce: src/main.rs
+
+/home/phirks/Documents/rust/advent_of_code/day_2/target/debug/deps/day_2-933a1194455727ce.d: src/main.rs
+
+src/main.rs:
