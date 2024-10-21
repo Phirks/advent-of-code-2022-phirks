d64e41f8a340a1a272661add9213427874292441
Nick Johnson <164326026+Phirks@users.noreply.github.com>
2024-10-21 19:22:44 -0400

    Delete day_1/target directory

--- day_1/target/debug/deps/day_1-d901feb9c0881b47.d
+++
@@ -1,5 +0,0 @@
-C:\Rust\advent_2022\day_1\target\debug\deps\libday_1-d901feb9c0881b47.rmeta: src/main.rs
-
-C:\Rust\advent_2022\day_1\target\debug\deps\day_1-d901feb9c0881b47.d: src/main.rs
-
-src/main.rs:
--- day_1/target/debug/deps/day_1.d
+++
@@ -1,5 +0,0 @@
-C:\Rust\advent_2022\day_1\target\debug\deps\day_1.exe: src/main.rs
-
-C:\Rust\advent_2022\day_1\target\debug\deps\day_1.d: src/main.rs
-
-src/main.rs:
